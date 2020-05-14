1615c55fca535899090765635eebdb0439d19cf3
fabianschmauder <fabian.schmauder@codecentric.de>
2020-05-14 12:03:07 +0200

    init project

---
+++ src/test/java/CalculationTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculationTest {
+
+  @Test
+  public void fivePlus3ShouldBe8() {
+    // GIVEN
+    int firstValue = 3;
+    int secondValue = 8;
+
+    //WHEN
+    int result = Calculation.add(firstValue, secondValue);
+
+    //THEN
+    assertEquals(11, result);
+  }
+}
